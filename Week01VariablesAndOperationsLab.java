d3388ba6bd7e78dc8e049e523df7011956ad9d73
Your Name <your.email@example.com>
2024-10-14 14:54:57 -0600

    first commit being made

---
+++ Week01VariablesAndOperationsLab.java
@@ -0,0 +1,265 @@
+package week01;
+
+import java.util.Scanner;
+
+public class Week01VariablesAndOperationsLab {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 1. Create a variable to hold the quantity of 
+				//		available plane seats left on a flight		
+//int availablePlaneSeats = 7;
+//System.out.println(availablePlaneSeats);
+				
+				// 2. Create a variable to hold the cost of groceries at checkout
+//double costOfGroceries = 53.46;
+//System.out.println(costOfGroceries);
+				
+				// 3. Create a variable to hold a person's middle initial
+//char middleInitial = 'm';
+//System.out.println(middleInitial);
+			
+				// 4. Create a variable to hold true if it's hot outside 
+				//		and false if it's cold outside
+//boolean hotOutside = true;
+//boolean coldOutside = false;
+//System.out.println(hotOutside);
+//System.out.println(coldOutside);
+				
+				// 5. Create a variable to hold a customer's first name
+//String customerName = "Meredith";
+//System.out.println(customerName);
+				
+				// 6. Create a variable to hold a street address
+//int streetNumber = 24313;
+//String streetName = "Ingebo";
+//String typeOfRoad = "Rd";
+//String streetAddress= (streetNumber + " " + streetName + " " + typeOfRoad);			
+//System.out.println(streetAddress);
+				// 7. Print all variables to the console
+			
+
+				// 8. A customer booked 2 plane seats, 
+				//		remove 2 seats from the available seats variable
+		//System.out.println(availablePlaneSeats - 2);	
+
+				// 9. Impulse candy bar purchase, add 2.15 to the grocery total
+				// 		costOfGroceries = costOfGroceries + 2.15;
+//System.out.println(costOfGroceries + 2.15);
+
+				// 10.  The birth certificate was printed incorrectly, 
+				//		change the middle initial to something else
+//middleInitial = 'b';
+//System.out.println(middleInitial);
+
+				// 11.  The season has changed, update the hot outside 
+				//			variable to be opposite of what it was
+//hotOutside = false;
+//System.out.println(hotOutside);
+
+				// 12.  Create a new variable called full name using the customer's first name, 
+				//			the middle initial, and a last name of your choice
+//String lastName = "Rothstein";
+//String fullName = (customerName + " " + middleInitial + " " + lastName);
+//System.out.println(fullName);
+
+				// 13.  Print a line to the console that introduces the customer and says they live
+				// 			at the address variable
+//System.out.println("This is" + " " + fullName + "." + " " + "She lives at" + " " + streetAddress + ".");
+//String employeeName = "Tony Stark";
+//System.out.println("Hi, my name is " + employeeName);
+//System.out.println(3==3);
+//int hat = 4;
+//int cat = 3;
+//System.out.println(hat == cat);
+//if (hat == 3) {
+	//System.out.println("HI!");
+//} //else {
+	//System.out.println("Not right.");
+//int age = 13;
+//if (age >= 16) {
+	//System.out.println("You can drive!");
+//} //else {
+	//System.out.println("You need to wait " + (16- age) + " year(s) till you can drive. Sorry.");
+//}
+//double costOfMedChair = 200.00;
+//if (costOfMedChair <= 150.00) {
+	//System.out.println("Let's buy that chair, baby!");
+//} else if (costOfMedChair <= 200) {
+//	System.out.println("eh... let's think about this");
+//} else { 
+//	System.out.println("Absolutely not buying that chair.");
+//}
+//char middleInt = 'M';
+//switch (middleInt) {
+//case 'K':
+//	System.out.println("that's not my middle initial");
+//	break;
+//case 'L':
+//	System.out.println("That's not it either");
+//	break;
+//case 'M':
+//	System.out.println("That's it!");
+//}
+//}
+//for (int i=0; i < 10; i = i+1) {
+//	System.out.println(i);
+//}
+//for (int i=1; i<=10; i++) {
+//	System.out.println(i);
+//}
+//	}
+//for (int i=1; i<=100; i++) {
+	//if (i%i == 0) {
+	//System.out.println("prime!");
+//}
+	//int y = 0;
+//while (y < 10) {
+	//System.out.println(y);
+	//y=y+2;
+//}
+//do {
+	//System.out.println(y);
+//	} while (y<10);
+
+//for (int cupsOfFlour = 1; cupsOfFlour <=5; cupsOfFlour++) {
+	//System.out.println("There are " + cupsOfFlour + " cups of flour in the bowl.");
+//Scanner sc = new Scanner(System.in);
+
+//System.out.print("Enter your name: ");
+//String name = sc.nextLine();
+//System.out.println("Welcome, " + name);
+//double total = 0;
+//int selection = 0;
+//while (selection != 4) {
+//System.out.println("Select an option.");
+//System.out.println("1) Add candybar to shopping cart: 1.99");
+//System.out.println("2) Add potato to shopping cart: 1.00");
+//System.out.println("3) Add cool hat to shopping cart: 12.99");
+//System.out.println("4) check out");
+//selection = sc.nextInt();
+
+
+
+//switch (selection) {
+//case 1 :
+	//total += 1.99;
+	//break;
+//case 2: 
+	//total += 1.00;
+	//break;
+//case 3:
+	//total += 12.99;
+//case 4:
+//	break;
+//	default:
+//		System.out.println("Please pick a valid option.");
+//}
+
+//}
+
+//System.out.println("Your current total is: " + total);
+//final int maxAttempts = 3;
+//int attempts = 0;
+//final String correctUsername = "Joh123";
+//final String correctPassword = "pw123";
+//Scanner scanner= new Scanner(System.in);
+//while (attempts < maxAttempts) {
+//	System.out.println("Enter username: ");
+//	String username = scanner.nextLine();
+//	
+//	System.out.println("Enter password: ");
+//	String password = scanner.nextLine();
+//	
+//if (username.equals(correctUsername) && password.equals(correctPassword))
+//{ System.out.println("Login successful!");
+//break;
+//} else {
+//	attempts++;
+//	System.out.println("Incorrect credentials. Attempts remaining: " + (maxAttempts-attempts));}
+//if (attempts >= maxAttempts) {
+//	System.out.println("Max login attempts reached.");
+//}}
+//scanner.close();
+//int counter = 0;
+//for (int i = 1; i < 100; i += 2) {
+ //   counter++;
+ //   System.out.println(counter);
+//}
+//int numToAdd = 8;       // Value to be added
+//int howManyTimes = 3;   // Number of times to add numToAdd
+//int sumResult = 0;      // Initial sum
+
+//int counters = 0;  
+//do {
+ //   sumResult += numToAdd;  // Add numToAdd to sumResult
+ //   counters++;              // Increment the counter after each iteration
+//} while (counters < howManyTimes);  // Loop runs howManyTimes times
+//
+//// Print the result
+//System.out.println("Final sumResult: " + sumResult);
+//
+//String userWord = "JAVA";
+//int wordLength = userWord.length();
+//if (wordLength<5) {
+//    System.out.println(userWord);
+//} else {
+ //   do {
+ //       System.out.println(userWord);
+ //       counter++;
+ //   } while (counter<wordLength);
+//}
+Scanner scanner= new Scanner(System.in);
+System.out.println("Userword: ");
+String userWord = scanner.nextLine();
+String longString= "";
+int lengthUW = userWord.length();
+for (int i=0; i<userWord.length(); i++) {
+	longString += userWord;
+}
+ System.out.println(longString);
+ 
+			
+			
+scanner.close();
+
+double moneyInWallet = 4.50;
+
+double costOfMilk = 1.99;
+
+if (moneyInWallet >= costOfMilk * 2) {
+
+  System.out.println("Let's buy two gallons.");
+
+} else if (moneyInWallet >= costOfMilk) {
+
+  System.out.println("Let's buy one gallon.");
+
+} else {
+
+  System.out.println("Way too expensive!");
+  
+for (int index=0; index<=99; index++) {
+	if(!(index%2==0)) {
+		System.out.println(index);
+	}
+}
+for (int index=0; index<=99; index++) {
+	if(!(index%2==0)) {
+		System.out.println(index);
+	}
+}
+}
+	}
+}
+
+
+				
+		
+	
+
+	
+
+
+
+
